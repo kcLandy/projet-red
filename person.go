3430bbd922730892cf73592a9b049faba57c7c2b
Lucas Martinez <lucas.martinez@ynov.com>
2023-09-18 15:29:06 +0200

    structure

---
+++ person.go
@@ -0,0 +1,21 @@
+package main
+
+type Personne struct {
+	name string
+	age int
+	rich bool 
+	strenght int 
+	agility int 
+	speed int 
+	gender string 
+	color string
+	HP int 
+	XP int 
+	hitpurcentage int 
+	Equipment string
+	defence int 
+	attack int 
+	shield int 
+	effects string
+	dodge int
+}
