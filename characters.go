bec93ec171e6ce035adfd906b4ff2ce15e0d07de
kcLandy <145334569+kcLandy@users.noreply.github.com>
2023-09-19 15:14:40 +0200

    Update characters.go

--- characters.go
+++ characters.go
@@ -2,29 +2,34 @@ package main
 
 import "fmt"
 
+type Classtype string
+
+const (
+	Warrior = "Warrior"
+	Archer = "Archer"
+	Mage = "Mage"
+)
 type character struct {
 	name string
-	class string 
+	class Classtype 
 	level int
 	maxlevel int
 	maxHP int 
 	currentHP int
-	inventory []int 
 }
 func main() {
-
 	player1 := character {
 		name : "Raff",
-		class : "warrior",
+		class : "Warrior",
 		level : 1,
 		maxlevel : 5,
 		currentHP : 100,
 		maxHP : 500,
 	}
-fmt.Println("character name :", player1.name)
-fmt.Println("character class :", player1.class)
-fmt.Println("character level :", player1.level)
-fmt.Println("character max level :", player1.maxlevel)
-fmt.Println("character HP :", player1.currentHP)
-fmt.Println("character max HP :", player1.maxHP)
+fmt.Println("character's name :", player1.name)
+fmt.Println("character's class :", player1.class)
+fmt.Println("character's level :", player1.level)
+fmt.Println("character's max level :", player1.maxlevel)
+fmt.Println("character's HP :", player1.currentHP)
+fmt.Println("character's max HP :", player1.maxHP)
 }
